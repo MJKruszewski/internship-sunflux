3e91faddaca0e2720c9f66cf73de6e8fe07b20f3
Maciej Kruszewski <m.kruszewski@sunflux.pl>
2021-08-05 22:02:39 +0200

    first train

---
+++ src/train/index.ts
@@ -0,0 +1,41 @@
+const zmienan = 0;
+let zmienna1 = 0;
+
+let zmiennaTypString : string = "String to wartość tekstowa";
+const zmiennaTypuNumber = 1.1;
+const zmiennaTypuNull = null;
+
+
+const tablicaStringow: Array<string> = [];
+const tablicaStringow2: string[] = [
+    "wartos1",
+    "wartosc2",
+    "wartosc3",
+    "wartosc4",
+    "wartosc5",
+];
+
+const tablicaWielowymiarowa: string[][] = [
+    [
+           "wartos11",
+           "wartosc12",
+           "wartosc13",
+           "wartosc14",
+           "wartosc15",
+    ],
+    [
+        "wartos21",
+        "wartosc22",
+        "wartosc23",
+        "wartosc24",
+        "wartosc25",
+    ],
+    [],
+    [],
+];
+
+
+const nowaZmienna: string[] = tablicaWielowymiarowa[0];
+const nowaZmienna2: string = nowaZmienna[4];
+
+const tmp: string = tablicaWielowymiarowa[1][2];
